daab9378c07ff12f0c2f2c584cb42928a1fbf285
brankej <johannes.branke@student.uibk.ac.at>
2018-05-23 18:03:32 +0200

    implemented coloured track to view the slope characteristics

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -16,6 +16,7 @@ let myMap = L.map("map", {
     fullscreenControl: true,});
 
 const etappe06Group = L.featureGroup();
+let overlaySteigung = L.featureGroup().addTo(myMap);
 // Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol
 // (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
 // WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
@@ -92,7 +93,7 @@ L.marker(ehrwald_marker, {icon: L.icon({
   iconAnchor : [16,37],
   popupAnchor : [0,-37],
 })
-}).addTo(etappe06Group).bindPopup(ehrwald_popup);
+}).addTo(overlaySteigung).bindPopup(ehrwald_popup);
 
 const scharnitz_marker = [47.388333,11.265]
 let scharnitz_popup = '<h1>Scharnitz </h1><a href="https://de.wikipedia.org/wiki/Scharnitz">Wikipedia</a>'
@@ -101,25 +102,53 @@ L.marker(scharnitz_marker, {icon: L.icon({
   iconAnchor : [16,37],
   popupAnchor : [0,-37],
 })
-}).addTo(etappe06Group).bindPopup(scharnitz_popup);
+}).addTo(overlaySteigung).bindPopup(scharnitz_popup);
 
 // Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
 let myMapControl  = L.control.layers({                // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
   //Auswahl der Hintergrundkarte
   "OpenStreetMap" : myLayers.osm,
   "Geoland Basemap" : myLayers.geolandbasemap,
-}, {
-  //overlay // Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
-  "Etappe 06" : etappe06Group,
   "Land Tirol Sommer" : gdi_summerGrp,
   "Land Tirol Winter" : gdi_winterGrp,
   "Land Tirol Orthophoto" : gdi_orthoGrp,
+}, {
+  //overlay // Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
+  //"Etappe 06" : etappe06Group,
+  "Steigung" : overlaySteigung,
 }, { //map control ausgeklappt lassen
   collapsed:false} );             // http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
 
-  let gpxTrack = new L.GPX('data/etappe06.gpx', {
-    async: true
-  }).addTo(etappe06Group);
+  //leaflet elevation
+
+  let hoehenprofil = L.control.elevation({
+      position: "bottomleft",
+      theme: "steelblue-theme", //default: lime-theme
+      width: 600,
+      height: 125,
+      margins: {
+          top: 10,
+          right: 20,
+          bottom: 30,
+          left: 50
+      },
+      useHeightIndicator: true, //if false a marker is drawn at map position
+      interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+      hoverNumber: {
+          decimalsX: 3, //decimals on distance (always in km)
+          decimalsY: 0, //deciamls on height (always in m)
+          formatter: undefined //custom formatter function may be injected
+      },
+      xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+      yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+      collapsed: false    //collapsed mode, show chart on click or mouseover
+    });
+  hoehenprofil.addTo(myMap);
+
+
+let gpxTrack = new L.GPX('data/etappe06.gpx', {
+  async: true,
+})//.addTo(etappe06Group);
   gpxTrack.on("loaded", function(evt) {
     myMap.fitBounds(evt.target.getBounds())
     let deep = evt.target.get_elevation_min().toFixed(0);
@@ -133,9 +162,60 @@ let myMapControl  = L.control.layers({                // http://leafletjs.com/re
     let Distanz = evt.target.get_distance().toFixed(0);
     Distanz = Distanz/1000 //to km
     document.getElementById("Distanz").innerHTML= Distanz;
+  });
+  gpxTrack.on("addline",function(evt){
+        hoehenprofil.addData(evt.line);
+        console.log(evt.line.getLatLngs()[0]);
+        console.log(evt.line.getLatLngs()[0].lat);
+        console.log(evt.line.getLatLngs()[0].lng);
+        console.log(evt.line.getLatLngs()[0].meta);
+        console.log(evt.line.getLatLngs()[0].meta.ele);
 
+        // alle Segmente der Steiungslinie hinzufügen
+        let gpxLinie = evt.line.getLatLngs();
+        for (let i = 1; i < gpxLinie.length; i++) {
+          let p1 = gpxLinie[i-1];
+          let p2 = gpxLinie[i];
+
+          let dist = myMap.distance(
+            [p1.lat,p1.lng],
+            [p2.lat,p2.lng]
+          );
+          //höhenunterschied rechnen
+          let delta = (p2.meta.ele -p1.meta.ele).toFixed(1);
+
+          //steigung in %
+          //let proz2 = 0;
+          //if (dist > 0) {
+          //  proz2 = (delta / dist * 100.0).toFixed(1);
+          //};
+          let proz = (dist > 0 ) ?  (delta / dist * 100.0).toFixed(1) : 0;
+
+
+          console.log(p1.lat,p1.lng,p2.lat,p2.lng, dist, delta, proz);
+
+          let farbe =
+            proz > 10  ? "#cb181d" :
+            proz > 6   ? "#fb6a4a" :
+            proz > 2   ? "#fcae91" :
+            proz > 0   ? "#fee5d9" :
+            proz > -2  ? "#edf8e9" :
+            proz > -6  ? "#bae4b3" :
+            proz > -10 ? "#74c476" :
+                          "#238b45";
+
+          let segment = L.polyline(
+            [
+            [p1.lat,p1.lng],
+            [p2.lat,p2.lng],
+          ],{
+              color: farbe,
+              weight : 10,
+
+            }
+          ).addTo(overlaySteigung)
+        }
   });
 
-  myMap.addControl(myMapControl);
-
-  myMap.addLayer(etappe06Group)
+myMap.addControl(myMapControl);
+myMap.addLayer(etappe06Group);
